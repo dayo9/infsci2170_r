050126527cb3d223620f6e440480615ee60778c4
dayo <dayodele@asu.edu>
2019-03-19 00:36:42 +0000

    edited ext_euc.c

--- hw6/ext_euc.c
+++ hw6/ext_euc.c
@@ -1,6 +1,8 @@
 #include <cstdio>
 #include <cassert>
 
+/*int a, b;*/
+
 int gccd(int x, int y, int &a, int &b) {
 	if (y == 0) {
 		a = 1;
@@ -9,17 +11,18 @@ int gccd(int x, int y, int &a, int &b) {
 		return x;
 	} else {
 		int a_t, b_t;
-		int r = gccd(y, x%y, a_t, b_t);
+		int r = gccd(y, x % y, a_t, b_t);
 		a = b_t;
-		b = a_t;
-		assert(a*x + b*y == r);
+		b = a_t - b_t * (x / y);
+		assert(a * x + b * y == r);
 		return r;
 	}
 }
 
 int main() {
 	int a, b;
-	int x = 12, y = 8;
+	int x =0, y = 0;
+	scanf(%d, x);
 	int r = gccd(x, y, a, b);
 	printf("%d * %d + %d * %d = %d\n", a, x, b, y, r);
 	printf("=> a = %d\n b = %d\n", a, b);
